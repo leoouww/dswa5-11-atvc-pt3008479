f2073c004c17f71fc0eec4e3e58c3d5130090387
Leonardo Castro <88807550+leoouww@users.noreply.github.com>
2022-04-14 20:56:20 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -3,21 +3,22 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = 'mongodb+srv://dswa5:<dswa5>@clusterleonardo.3fhkc.mongodb.net/ClusterLeonardo?retryWrites=true&w=majority';
+const url = 'mongodb://dswa5:dswa5@clusterleonardo-shard-00-00.3fhkc.mongodb.net:27017,clusterleonardo-shard-00-01.3fhkc.mongodb.net:27017,clusterleonardo-shard-00-02.3fhkc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-4go5n9-shard-0&authSource=admin&retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'PROJECT 0';
+const dbName = 'ifsp';
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('documents');
+  const collection = db.collection('contatos');
 
   // the following code examples can be pasted here...
-
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
   return 'done.';
 }
 
